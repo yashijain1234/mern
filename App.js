0b8a7602de6d33f2317e2471d0e8d0b06b62892b
yashijain1234 <73181774+yashijain1234@users.noreply.github.com>
2021-12-03 10:29:52 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,16 @@
+
+import './App.css';
+import Apicallingclass1 from './mycomponents/Apicallingclass1'
+import Apicallingusinghooks from './mycomponents/Apicallingusinghooks';
+
+function App() {
+  return (
+    <div className="App">
+      <h2> Api Calling </h2>
+    <Apicallingclass1></Apicallingclass1>
+    <Apicallingusinghooks></Apicallingusinghooks>
+    </div>
+  );
+}
+
+export default App;
